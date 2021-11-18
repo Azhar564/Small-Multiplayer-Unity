04ef30b7791413d065b43d5263a54489e9bd65d8
Azhar564 <azharramadhan7777@gmail.com>
2021-11-18 22:30:04 +0700

    update Animation loading

--- Multiplayer unity/Multiplayer Test/Assets/Scripts/Connecting/ToServer.cs
+++ Multiplayer unity/Multiplayer Test/Assets/Scripts/Connecting/ToServer.cs
@@ -3,21 +3,35 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using Photon.Pun;
+using UnityEngine.UI;
 
 public class ToServer : MonoBehaviourPunCallbacks
 {
+    public Text connectingTxt;
+
+    private int i;
+    private string loadText;
     // Start is called before the first frame update
     void Start()
     {
         PhotonNetwork.ConnectUsingSettings();
+        loadText = connectingTxt.text;
+        StartCoroutine(ConnectAnim(.2f));
     }
 
-    // Update is called once per frame
-    void Update()
+    private IEnumerator ConnectAnim(float time)
     {
-        
+        while (true)
+        {
+            i = i > 5 ? 1 : i + 1;
+            connectingTxt.text = loadText;
+            for (int k = 0; k < i; k++)
+            {
+                connectingTxt.text += ".";
+            }
+            yield return new WaitForSeconds(time);
+        }
     }
-
 
     public override void OnConnectedToMaster()
     {
